7cd4bd2e35106c5a0bb7023577279819f59c08fc
Sven Steinert <svensteinert96@gmail.com>
2022-03-29 00:55:51 +0200

    static pdf viewer

---
+++ reference/js/player.js
@@ -0,0 +1,54 @@
+var pdfv_buffer;
+ 
+ document.getElementById('inputfile_pdfv').addEventListener('change', function() {
+            var fr=new FileReader();
+            fr.onload=function(){
+                pdfv_buffer = fr.result;
+            }
+            fr.readAsArrayBuffer(this.files[0]);
+            display_pdf();
+        })
+function display_pdf() {
+var url = 'https://raw.githubusercontent.com/unconsciou5/PDFV/master/examples/document.pdf';
+
+// Loaded via <script> tag, create shortcut to access PDF.js exports.
+var pdfjsLib = window['pdfjs-dist/build/pdf'];
+
+// The workerSrc property shall be specified.
+pdfjsLib.GlobalWorkerOptions.workerSrc = '//mozilla.github.io/pdf.js/build/pdf.worker.js';
+
+// Asynchronous download of PDF
+var loadingTask = pdfjsLib.getDocument(url);
+loadingTask.promise.then(function(pdf) {
+  console.log('PDF loaded');
+  
+  // Fetch the first page
+  var pageNumber = 1;
+  pdf.getPage(pageNumber).then(function(page) {
+    console.log('Page loaded');
+    
+    var scale = 1.5;
+    var viewport = page.getViewport({scale: scale});
+
+    // Prepare canvas using PDF page dimensions
+    var canvas = document.getElementById('the-canvas');
+    var context = canvas.getContext('2d');
+    canvas.height = viewport.height;
+    canvas.width = viewport.width;
+
+    // Render PDF page into canvas context
+    var renderContext = {
+      canvasContext: context,
+      viewport: viewport
+    };
+    var renderTask = page.render(renderContext);
+    renderTask.promise.then(function () {
+      console.log('Page rendered');
+    });
+  });
+}, function (reason) {
+  // PDF loading error
+  console.error(reason);
+});
+}
+ 
